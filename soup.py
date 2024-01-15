bfcd9948d3f49ddd10980eef46fc1f55f5353f2e
Steffan Burnette <burnettesteffansep7@gmail.com>
2024-01-14 19:36:56 -0500

    new

--- soup.py
+++ soup.py
@@ -11,7 +11,8 @@ article = soup.find_all('div') #Contains the terms of service
 
 #print(soup.title.get_text())
 fixed_text=""
-#Removes all the tags
+
+#Removes all the tags and adds the text to the fixed_texts variable
 for x in article:
     fixed_text+=x.get_text()
 
@@ -19,7 +20,7 @@ for x in article:
 
 print(article)    
 
-
+   
 #Summarization nlp model that can handle a large quantity of text
 hf_name = 'pszemraj/led-large-book-summary'
 
@@ -46,3 +47,10 @@ print(result)
 #Token indices sequence length is longer than the specified maximum sequence length for this model (49267 > 
 #16384). Running this sequence through the model will result in indexing errors
 #Will need to make sure that the text being inputted fits the models restraints
+
+'''
+from urllib import request,response
+url = "http://www.example.com"
+response=request.Request(url,headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'})
+print(response.get_full_url())
+'''
